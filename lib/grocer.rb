6ab7f0b48f6cec87df26fcc561031dc1c8bea9f7
David Polcari <dpolcari18@gmail.com>
2021-01-06 22:47:14 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,30 +1,21 @@
 require 'pry'
 
 def find_item_by_name_in_collection(name, collection)
-  #QUESTION: why does solution in part 2 include index increments?
-  collection.detect do |value|
-    value[:item]==name
+  collection.detect do |index_value|
+    index_value[:item]==name
   end
 end
 
 def consolidate_cart(cart)
-  #QESTION: why does solution in part 2 include index increments?
   receipt=[]
-  cart.each do |cart_items|
-    current_item=find_item_by_name_in_collection(cart_items[:item], receipt)
-    if current_item
-      receipt.each do |receipt_items|
-        if receipt_items[:item]==cart_items[:item]
-          receipt_items[:count]+=1
-        end
-      end
+  cart.each do |index|
+    if !find_item_by_name_in_collection(index[:item], receipt)
+      index[:count]=1
+      receipt.push(index)
     else
-      cart_items[:count]=1
-      receipt.push(cart_items)
+      index[:count]+=1
     end
   end
-  #binding.pry
-  return receipt
 end
 
   
